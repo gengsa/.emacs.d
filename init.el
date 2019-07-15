ddb8c06b2ece56bc056c3d43821b9c692ec1e855
gengas <342796923@qq.com>
2019-07-15 19:42:26 +0800

    add auto install packages, some basic customizations

--- init.el
+++ init.el
@@ -4,14 +4,20 @@
 ;; 关闭滚动条，在窗口版中才有
 ;; (scroll-bar-mode -1)
 
-;; 显示行号
-(global-linum-mode 1)
-
 ;; 更改光标显示样式，这一行为什么还跑步起来不知道，是不是因为只有在窗口版下才生效？
 ;; (setq-default cursor-type 'bar)
 
+;; 显示行号
+(global-linum-mode 1)
+
 ;; 高亮当前行
 (global-hl-line-mode 1)
+
+;; 显示最近打开文件
+(recentf-mode 1)
+
+;; 显示匹配的括号，但是好像只显示了圆括号
+(show-paren-mode 1)
 
 ;; 快速打开配置文件
 (defun open-init-file()
@@ -25,38 +31,48 @@
 (delete-selection-mode 1)
 
 (require 'package)
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (proto (if no-ssl "http" "https")))
-  (when no-ssl
-    (warn "\
-Your version of Emacs does not support SSL connections,
-which is unsafe because it allows man-in-the-middle attacks.
-There are two things you can do about this warning:
-1. Install an Emacs version that does support SSL and be safe.
-2. Remove this warning from your init file so you won't see it again."))
-  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
-  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
-  (when (< emacs-major-version 24)
-    ;; For important compatibility libraries like cl-lib
-    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
 (package-initialize)
+;; 使用中国镜像
+(setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
+			 ("melpa" . "http://elpa.emacs-china.org/melpa/")))
+
+(require 'cl)
+
+(defvar my/packages '(
+		      ;; --- Auto-completion ---
+		      company
+
+		      ;; --- Smart parentheses ---
+		      smartparens
+		      ) "Default packages")
+
+;; 这一行是为了替换掉 custom-set-variables 中自动生成的 '(package-selected-packages (qoute company)) 。我们自己安装的插件不应该声明两遍。
+;; 发现，如果直接在那里修改成为 ‘my/packages，在触发自动装插件，或者 package-autoremove后，他会重写那一行。用这个 setq 可以解决这个问题。
+;; 但是发现还有小瑕疵，当这个变量为空列表时，还是有之前的问题，但也还好，不要空就好了。
+(setq package-selected-packages my/packages)
+
+(defun my/packages-installed-p ()
+  (loop for pkg in my/packages
+	when (not (package-installed-p pkg)) do (return nil)
+	finally (return t)))
+
+;; this line equals (if (my/packages-installed-p) nil
+(unless (my/packages-installed-p)
+  (message "%s" "Refreshing package database...")
+  (package-refresh-contents)
+  (dolist (pkg my/packages)
+    (when (not (package-installed-p pkg))
+      (package-install pkg))))
 
 ;; 开启全局 Company 补全
-;; (global-company-mode 1)
+(global-company-mode 1)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (company))))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
