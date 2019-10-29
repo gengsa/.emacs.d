181608e0af87cf28af40c9438e69b0b66413be4f
gengas <342796923@qq.com>
2019-10-29 09:52:33 +0800

    rename init-ui.el to init-editor.el

--- init.el
+++ init.el
@@ -1,18 +1,12 @@
-;; 显示最近打开文件
-(recentf-mode 1)
-
 ;; 显示匹配的括号，但是好像只显示了圆括号
 (show-paren-mode 1)
-
-;; 这个 mode 可以在选择状态下，输入的文字时将直接替换被选中的文字
-(delete-selection-mode 1)
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
 (require 'init-packages)
-(require 'init-ui)
 ;; common functions and keybinds
 (require 'init-utils)
+(require 'init-editor)
 
 ;; 开启全局 Company 补全
 (global-company-mode 1)
--- lisp/init-editor.el
+++ lisp/init-editor.el
@@ -1,10 +1,13 @@
-;;; init-ui.el --- UI optimizations
+;;; init-editor.el --- Editor optimizations
 
 (when (fboundp 'tool-bar-mode)
   (tool-bar-mode -1))
 
 (when (fboundp 'scroll-bar-mode)
   (scroll-bar-mode -1))
+
+;; recent files
+(recentf-mode t)
 
 ;; disable the annoying bell ring
 ;; (setq ring-bell-function 'ignore)
@@ -20,7 +23,13 @@
 ;; 字数统计
 (size-indication-mode t)
 
+(delete-selection-mode t)
+
+;; add final newline, and delete trailing whitespace/lines
+(setq require-final-newline t)
+(setq delete-trailing-lines t)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
 (fset 'yes-or-no-p 'y-or-n-p)
 
-(provide 'init-ui)
-
+(provide 'init-editor)
